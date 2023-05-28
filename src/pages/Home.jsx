7d80159d24f454bb81cee32d27203537e5416eed
alejandroCano <alekandev@gmail.com>
2023-05-28 12:02:07 -0500

    home view and css with media 720px

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../styles/home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homeContent'>
+      <div className="homeImage" /> {/* division sin contenido */}
+      <div className="infoContent">
+        <span className="label textFixed fixed1">MI NOMBRE ES ALEJANDRO</span>
+        <span className="label textFixed fixed2">Y SOY</span>
+        <span className="label textVariable">DESARROLLADOR DE SOFTWARE</span>
+        <span className="welcome1">Eres bienvenid@ a </span>
+        <span className="welcome2">mi portafolio  de seguimiento personal</span>
+        <div className="lineSeparate" /> {/* division sin contenido */}
+        <button className="contactMe">Contactame</button>
+      </div>
+    </div>
   )
 }
 
